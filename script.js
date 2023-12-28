80106c6f0d8804e1f63f7adfa7db2d11a6740746
TSIBO <81839976+Tsi-Bo@users.noreply.github.com>
2023-12-28 10:49:10 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,12 +2,25 @@
 
 //Setting the status of the game
 let isGamePlaying = true;
-
 //Setting the status of the current player
 let currentPlayer = 1;
+//Setting the scores
+let player1Score = 0;
+let player2Score = 0;
+let tiesScore = 0;
 
+//DOM Elements
 const squares = document.querySelectorAll(".square");
 const statusGame = document.querySelector(".status");
+const resetButton = document.querySelector(".reset-btn");
+const player1Text = document.querySelector(".player1");
+const player2Text = document.querySelector(".player2");
+const tiesText = document.querySelector(".ties");
+
+// On load
+player1Text.textContent = `PLAYER 1 : ${player1Score}`;
+player2Text.textContent = `PLAYER 2 : ${player2Score}`;
+tiesText.textContent = `TIES : ${player1Score}`;
 
 squares.forEach((item) => {
   item.addEventListener("click", function () {
@@ -21,11 +34,35 @@ squares.forEach((item) => {
 
       if (checkWin()) {
         statusGame.textContent = `Player${currentPlayer} wins the game !`;
+        if (currentPlayer === 1) {
+          player1Score += 1;
+          player1Text.textContent = `PLAYER 1 : ${player1Score}`;
+        } else {
+          player2Score += 1;
+          player2Text.textContent = `PLAYER 2 : ${player2Score}`;
+        }
       } else if (checkDraw()) {
         statusGame.textContent = `It's a draw !`;
+        tiesScore += 1;
+        tiesText.textContent = `TIES : ${tiesScore}`;
       }
     }
   });
+});
+
+// Reseting the game
+resetButton.addEventListener("click", function () {
+  // Clearing every square
+  squares.forEach((item) => {
+    item.innerHTML = "";
+  });
+  // Reseting other variables
+  isGamePlaying = true;
+  currentPlayer = 1;
+  statusGame.textContent = `Player ${currentPlayer}'s turn.`;
+  let player1Score = 0;
+  let player2Score = 0;
+  let tiesScore = 0;
 });
 
 function checkWin() {
